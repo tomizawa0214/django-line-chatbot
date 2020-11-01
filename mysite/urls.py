54bbad60f10c985ffb2dd2ce8a8e405876d34499
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-11-01 19:54:57 +0900

    mysite urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('line/', include('app.urls', namespace='app')),
 ]
