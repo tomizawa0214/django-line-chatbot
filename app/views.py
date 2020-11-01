6b0bbc5720ecd66e2e59edb0713307d1d14886cd
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-11-01 18:42:18 +0900

    トークン

--- app/views.py
+++ app/views.py
@@ -13,6 +13,9 @@ import os
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
 
 @csrf_exempt
 def callback(request):
